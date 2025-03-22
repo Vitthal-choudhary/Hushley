2b8247e94f69268645ab8f61e4937dd7c8d2f406
Vitthal-Choudhary <vitthal.choudhary.14@gmail.com>
2025-03-22 12:58:32 +0530

    profile

--- app/src/main/java/com/example/hushley/Screens/profile_screen.kt
+++ app/src/main/java/com/example/hushley/Screens/profile_screen.kt
@@ -1,23 +1,31 @@
 package com.example.hushley.Screens
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.compose.material3.Button
+import androidx.navigation.compose.rememberNavController
 import com.example.hushley.R
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.ExitToApp
 import com.example.hushley.ui.theme.*
 
 @Composable
@@ -48,41 +56,100 @@ fun Profile(navController: NavHostController) {
                     )
                 }
                 item {
-                    ProfileCard("Name:", "User Name")
+                    Text(
+                        text = "@User",
+                        style = MaterialTheme.typography.headlineLarge,
+                        textAlign = TextAlign.Center,
+                        color = primaryDarkMediumContrast
+                    )
                 }
                 item {
-                    ProfileCard("Age:", "20")
+                    ProfileCard(
+                        mapOf(
+                            "Name" to "User Name",
+                            "Age" to "20",
+                            "Sex" to "Male",
+                            "Phone" to "+91 9999999999",
+                            "Mail" to "abc@gmail.com",
+                            "Address" to "India"
+                        )
+                    )
                 }
                 item {
-                    ProfileCard("Sex:", "Male")
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .fillMaxWidth(0.5f)
+                            .padding(10.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Edit,
+                            contentDescription = "Edit",
+                            modifier = Modifier.padding(end = 5.dp)
+                        )
+                        Text(
+                            text = "Edit Details"
+                        )
+                    }
                 }
-                item {
-                    ProfileCard("Phone:", "+91 9999999999")
+                item{
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .fillMaxWidth(0.5f)
+                            .padding(10.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ExitToApp,
+                            contentDescription = "Log out",
+                            modifier = Modifier.padding(end=5.dp)
+                        )
+                        Text(
+                            text = "Log Out"
+                        )
+                    }
                 }
-                item {
-                    ProfileCard("Mail:", "abc@gmail.com")
-                }
-                item {
-                    ProfileCard("Address:", "India")
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun ProfileCard(label: String, value: String) {
+fun ProfileCard(details: Map<String, String>) {
     Card(
         modifier = Modifier
             .padding(8.dp)
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .border(1.dp, primaryDark, RoundedCornerShape(16.dp)),
         shape = RoundedCornerShape(16.dp),
-        border = BorderStroke(1.dp, color = primaryDark),
         colors = CardDefaults.cardColors(containerColor = secondaryDark)
     ) {
-        Row(modifier = Modifier.padding(16.dp)) {
-            Text(text = "$label", fontWeight = FontWeight.Bold, fontSize = 18.sp, color = scrimLightHighContrast)
-            Text(text = " $value", fontSize = 18.sp, color = scrimLightHighContrast)
+        Column(modifier = Modifier.padding(16.dp)) {
+            details.forEach { (label, value) ->
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = "$label:",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        color = scrimLightHighContrast,
+                        modifier = Modifier.weight(1f)
+                    )
+                    Text(
+                        text = value,
+                        fontSize = 18.sp,
+                        color = scrimLightHighContrast,
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+            }
         }
     }
+}
+
+@Preview
+@Composable
+fun ProfilePreview() {
+    val navController = rememberNavController()
+    Profile(navController = navController)
 }
